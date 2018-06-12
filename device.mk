0b8c945052b20676ba1fe536f8b3153502cf915a
Tim Clark <tim.clark.82@gmail.com>
2018-06-12 11:32:17 -0700

    hammerhead: Set vendor security patch level
    From ro.build.version.security_patch in build.prop from the latest 6.0.1 factory image
    
    Change-Id: I127e8997716048b3587bf863b99f95f3f487f9e5

--- device.mk
+++ device.mk
@@ -437,3 +437,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 $(call inherit-product-if-exists, hardware/qcom/msm8x74/msm8x74.mk)
 $(call inherit-product-if-exists, vendor/qcom/gpu/msm8x74/msm8x74-gpu-vendor.mk)
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4339/device-bcm.mk)
+
+# Vendor security patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lineage.build.vendor_security_patch=2016-10-05
